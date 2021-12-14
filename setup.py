2fefa81dacb7a7074f0d0f9355e3c40eb5628cbc
Jocelin Pitteloud <jin.pitteloud@gmail.com>
2021-12-14 11:59:31 +0100

    Add setup clean + requirements.txt auto

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-with open('requirements.txt', mode='r') as f:
-    req = f.readlines()
+with open('requirements.txt', encoding='utf8') as f:
+    req = f.read().splitlines()
 
 setup(
     name='MusicOnto',
@@ -9,5 +9,5 @@ setup(
     description='Python package to extract Sentics values from text and adding them into an Ontology',
     author='Zoee Maryam & Pitteloud Jocelin',
     packages=['MusicOnto'],
-    install_requires=[files for files in req],
+    install_requires=req
 )
